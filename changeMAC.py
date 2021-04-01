277612f39f3f97d30aeb4d0b464567f89257b20d
Leonhard Masche <67312077+leonhma@users.noreply.github.com>
2021-04-01 12:40:37 +0200

    added manual change functionality

--- changeMAC.py
+++ changeMAC.py
@@ -1,10 +1,10 @@
-# pylama:ignore=E501
 from collections import defaultdict
 from json import dump, load
-from tkinter import Tk, Frame, StringVar, IntVar, OptionMenu, Label, Checkbutton, Button, Entry
-from winreg import HKEY_LOCAL_MACHINE, ConnectRegistry, OpenKey, QueryInfoKey, EnumKey, QueryValueEx
-
-from pytimeparse.timeparse import timeparse
+from random import randint
+from tkinter import Tk, Frame, StringVar, IntVar, OptionMenu, Label, \
+    Checkbutton, Button, Entry
+from winreg import HKEY_LOCAL_MACHINE, REG_SZ, ConnectRegistry, OpenKey, \
+    QueryInfoKey, EnumKey, QueryValueEx, SetValue, DeleteValue
 
 LOCAL_REGISTRY = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
 
@@ -13,7 +13,7 @@ def vivdict(preload=None):
     if preload is None:
         preload = {}
     for key, value in preload.items():
-        if isinstance(value, dict):  # Convert all `dict`s to `defaultdict`
+        if isinstance(value, dict):
             preload[key] = vivdict(value)
     return defaultdict(vivdict, preload)
 
@@ -21,9 +21,12 @@ def vivdict(preload=None):
 class MAC():
     def __init__(self, registry):
         self._registry = registry
-        self._adaptersKey = OpenKey(self._registry, r"SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}")
+        self._adaptersKey = OpenKey(self._registry, (r'SYSTEM\CurrentControlSe'
+                                                     r't\Control\Class\{4d36e9'
+                                                     r'72-e325-11ce-bfc1-08002'
+                                                     r'be10318}'))
 
-    def getNetworkAdapters(self):
+    def getNetworkAdapters(self) -> dict:
         adapters = {}
         for i in range(QueryInfoKey(self._adaptersKey)[0]):
             try:
@@ -43,14 +46,20 @@ class MAC():
                 break
         return adapters
 
-    def setMAC(self, adapterID, newMAC):
-        pass  # todo
+    def setMAC(self, adapterID: str, newMAC: str):
+        adapterKey = OpenKey(self._adaptersKey, adapterID)
+        SetValue(adapterKey, 'NetworkAddress', REG_SZ, newMAC)
 
-    def deleteMAC(self, adapterID):
-        pass  # todo
+    def deleteMAC(self, adapterID: str):
+        adapterKey = OpenKey(self._adaptersKey, adapterID)
+        DeleteValue(adapterKey, 'NetworkAddress')
 
-    def generateMAC(self, OUI=None):
-        pass  # todo
+    def generateMAC(self, OUI: str):
+        return f"{OUI}%02x%02x%02x".upper() % (
+            randint(0, 255),
+            randint(0, 255),
+            randint(0, 255),
+        )
 
 
 class App(Frame):
@@ -75,20 +84,18 @@ class App(Frame):
         topframe.grid(sticky='nesw')
         topframe.grid_rowconfigure(0, weight=1)
         topframe.grid_rowconfigure(1, weight=1)
-        topframe.grid_columnconfigure(0, weight=2)
-        topframe.grid_columnconfigure(1, weight=1)
+        topframe.grid_columnconfigure(0, weight=1)
         bottomframe = Frame(self.parent, relief='raised', borderwidth=1)
         bottomframe.grid_columnconfigure(0, weight=5)
         bottomframe.grid_columnconfigure(1, weight=6)
         bottomframe.grid_rowconfigure(0, weight=1)
         bottomframe.grid_rowconfigure(1, weight=1)
         bottomframe.grid(row=1, column=0, sticky='nesw')
-        brightframe = Frame(bottomframe)
-        brightframe.grid_rowconfigure(0, weight=1)
-        brightframe.grid_rowconfigure(1, weight=1)
-        brightframe.grid_columnconfigure(0, weight=1)
-        brightframe.grid_columnconfigure(1, weight=1)
-        brightframe.grid(row=0, column=1, rowspan=2, sticky='nesw')
+        blabelframe = Frame(bottomframe)
+        blabelframe.grid_rowconfigure(0, weight=1)
+        blabelframe.grid_columnconfigure(0, weight=1)
+        blabelframe.grid_columnconfigure(1, weight=1)
+        blabelframe.grid(row=0, column=1, sticky='nesw')
 
         lbl = Label(topframe, text='Network interface')
         lbl.grid(row=0, column=0)
@@ -99,26 +106,29 @@ class App(Frame):
         self._opts = tuple(self._optionsdict.keys())
         OptionMenu(topframe, self._tkvar1, *self._opts).grid(row=1, column=0)
 
-        Button(topframe, text='reset settings', command=self._cbReset).grid(row=1, column=1)
+        Button(bottomframe,
+               text='reset settings',
+               command=self._cbReset).grid(row=1, column=1)
 
         self._tkvar2 = IntVar(self.parent)
         self._tkvar2.trace('w', self._cbCheckbutton)
-        Checkbutton(bottomframe, text='change on restart', variable=self._tkvar2).grid(row=0, column=0)
+        Checkbutton(bottomframe,
+                    text='change on restart',
+                    variable=self._tkvar2).grid(row=0, column=0)
 
-        Button(bottomframe, text='change MAC', command=self._cbChange).grid(row=1, column=0)
+        Button(bottomframe,
+               text='change MAC',
+               command=self._cbChange).grid(row=1, column=0)
 
-        Label(brightframe, text='OUI').grid(row=0, column=0, padx=7, sticky='e')
-
-        Label(brightframe, text='Expiry time').grid(row=1, column=0, padx=7, sticky='e')
+        Label(blabelframe,
+              text='OUI').grid(row=0, column=0, padx=7, sticky='e')
 
         self._tkvar3 = StringVar(self.parent)
         self._tkvar3.trace('w', self._cbOUI)
         vcmd = (self.register(self._validateOUI), '%P', '%S')
-        Entry(brightframe, textvariable=self._tkvar3, validate='key', vcmd=vcmd).grid(row=0, column=1)
-
-        self._tkvar4 = StringVar(self.parent)
-        self._tkvar4.trace('w', self._cbExTime)
-        Entry(brightframe, textvariable=self._tkvar4).grid(row=1, column=1)
+        Entry(blabelframe,
+              textvariable=self._tkvar3,
+              validate='key', vcmd=vcmd).grid(row=0, column=1)
 
         self._tkvar1.set(self._opts[0])
 
@@ -131,26 +141,21 @@ class App(Frame):
     def _cbOUI(self, *args):
         text = self._tkvar3.get()
         if(len(text) == 6):
-            self._config['adapterSettings'][self._currentAdapter]['OUI'] = text[0:6]
+            var = self._config['adapterSettings'][self._currentAdapter]
+            var['OUI'] = text[0:6]
 
     def _cbDropdown(self, *args):
         self._currentAdapter = self._optionsdict[self._tkvar1.get()]
 
-        var = self._config['adapterSettings'][self._currentAdapter]['changeOnRestart']
+        var = self._config['adapterSettings'][self._currentAdapter]
+        var = var['changeOnRestart']
         self._tkvar2.set(var if var != {} else 0)
         var = self._config['adapterSettings'][self._currentAdapter]['OUI']
-        self._tkvar3.set(var if var != {} else '')
-        var = self._config['adapterSettings'][self._currentAdapter]['expiredDelta']
-        self._tkvar4.set(var if var != {} else '')
+        self._tkvar3.set(var if var != {} else '000C2A')
 
     def _cbCheckbutton(self, *args):
-        self._config['adapterSettings'][self._currentAdapter]['changeOnRestart'] = self._tkvar2.get()
-
-    def _cbExTime(self, *args):
-        delta = self._tkvar4.get()
-        if timeparse(delta) is None:
-            return
-        self._config['adapterSettings'][self._currentAdapter]['expiredDelta'] = delta
+        var = self._config['adapterSettings'][self._currentAdapter]
+        var['changeOnRestart'] = self._tkvar2.get()
 
     def _cbReset(self, *args):
         self._config['adapterSettings'][self._currentAdapter] = vivdict()
@@ -158,7 +163,8 @@ class App(Frame):
         self._mac.deleteMAC(self._currentAdapter)
 
     def _cbChange(self, *args):
-        newMAC = self._mac.generateMAC(self._config['adapterSettings'][self._currentAdapter]['OUI'])
+        var = self._config['adapterSettings'][self._currentAdapter]['OUI']
+        newMAC = self._mac.generateMAC(var)
         self._mac.setMAC(self._currentAdapter, newMAC)
 
     def saveConfig(self, *args):
@@ -170,6 +176,6 @@ class App(Frame):
 if __name__ == '__main__':
     root = Tk()
     root.title("changeMAC - development version")
-    root.geometry('400x150')
+    root.geometry('350x150')
     App(root)
     root.mainloop()
